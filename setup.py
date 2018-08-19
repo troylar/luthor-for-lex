302a3a7b7aa87a1753c32fa3bb2a899f633eb8e7
Troy Larson <troylar@gmail.com>
2018-08-19 16:18:57 -0400

    Move cli

--- setup.py
+++ setup.py
@@ -3,22 +3,26 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name="luthor-for-lex",
-    version="0.0.1",
+    version="0.0.2",
     author="Troy Larson",
     author_email="troylar@gmail.com",
     description="Easy Lex bot manager",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/troylar/luthor-for-lex",
-    packages=setuptools.find_packages(),
+    install_required=required,
+    packages=["speaker", "lex"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    entry_points = {
-        'console_scripts': ['luthor=cli:cli'],
+    entry_points={
+        'console_scripts': ['luthor=cli.main:cli'],
     }
 )
