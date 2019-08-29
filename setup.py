350c0649de4bcb35dec28f2ef4d7c776d512d46d
Troy Larson <troylar+github@pm.me>
2019-08-29 15:00:54 -0400

    Preparing release 0.0.16

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="luthor-for-lex",
-    version='0.0.16.dev0',
+    version='0.0.16',
     author="Troy Larson",
     author_email="troylar@gmail.com",
     description="Easy Lex bot manager",
